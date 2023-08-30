6d6f36863d437efd2902d5eb3ad98e0707cab19d
Erin Sheldon <esheldon@sdfrome002.sdf.slac.stanford.edu>
2023-08-30 10:33:24 -0700

    running a sim to get something we can process
    Currently the real cell coadds don't have everything we need

--- python/lsst/drp/tasks/metadetection_shear.py
+++ python/lsst/drp/tasks/metadetection_shear.py
@@ -20,6 +20,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from __future__ import annotations
+import numpy as np
 
 from lsst.daf.butler import DataCoordinate, DatasetRef
 from lsst.pipe.base.connectionTypes import BaseInput, Output
@@ -118,6 +119,8 @@ class MetadetectionShearConfig(
 ):
     """Configuration definition for MetadetectionShearTask."""
 
+    from lsst.meas.base import SkyMapIdGeneratorConfig
+
     required_bands = ListField[str](
         "Bands expected to be present.  Cells with one or more of these bands "
         "missing will be skipped.  Bands other than those listed here will "
@@ -127,6 +130,8 @@ class MetadetectionShearConfig(
         default=["r"],
         optional=False,
     )
+
+    idGenerator = SkyMapIdGeneratorConfig.make_field()
 
     # TODO: expose more configuration options here.
 
@@ -231,13 +236,20 @@ class MetadetectionShearTask(PipelineTask):
         # Read the coadds and put them in the order defined by
         # config.required_bands (note that each MultipleCellCoadd object also
         # knows its own band, if that's needed).
+
+        # TODO make this work
+        idGenerator = self.config.idGenerator.apply(qc.quantum.dataId)
+        seed = idGenerator.catalog_id
+
         coadds_by_band = {
             ref.dataId["band"]: qc.get(ref) for ref in inputRefs.input_coadds
         }
-        outputs = self.run([coadds_by_band[b] for b in self.config.required_bands])
+        outputs = self.run(
+            [coadds_by_band[b] for b in self.config.required_bands], seed,
+        )
         qc.put(outputs, outputRefs)
 
-    def run(self, patch_coadds: Sequence[MultipleCellCoadd]) -> Struct:
+    def run(self, patch_coadds: Sequence[MultipleCellCoadd], seed: int) -> Struct:
         """Run metadetection on a patch.
 
         Parameters
@@ -246,6 +258,9 @@ class MetadetectionShearTask(PipelineTask):
                 `~lsst.cell_coadds.MultipleCellCoadd` ]
             Per-band, per-patch coadds, in the order specified by
             `MetadetectionShearConfig.required_bands`.
+        seed: int
+            A seed for random number generator, used for simulation, and/or
+            fitting.
 
         Returns
         -------
@@ -255,11 +270,28 @@ class MetadetectionShearTask(PipelineTask):
             - ``object_catalog`` [ `pyarrow.Table` ]: the output object
               catalog for the patch, with schema equal to `object_schema`.
         """
+
+        # TODO figure out how to use a config to switch to simulation mode
+        # in that case also need to fake the id info
+        rng = np.random.RandomState(seed)
+        idstart = 0
+
         single_cell_tables: list[pa.Table] = []
         for single_cell_coadds in zip(
             *[patch_coadd.cells.values() for patch_coadd in patch_coadds], strict=True
         ):
-            single_cell_tables.append(self.process_cell(single_cell_coadds))
+            self._log_ids(single_cell_coadds[0])
+
+            res = self.process_cell(single_cell_coadds, rng, simulate=True)
+            # TODO figure out how to make the object ids
+            if len(res) > 0:
+                res['id'] = np.arange(idstart, idstart + len(res))
+                da = _dictify(res)
+                table = pa.Table.from_pydict(da, self.object_schema)
+
+                single_cell_tables.append(table)
+                idstart += len(res)
+
         # TODO: if we need to do any cell-overlap-region deduplication here
         # (instead of purely in science analysis code), this is where'd it'd
         # happen.
@@ -267,7 +299,10 @@ class MetadetectionShearTask(PipelineTask):
             object_catalog=pa.concat_tables(single_cell_tables),
         )
 
-    def process_cell(self, single_cell_coadds: Sequence[SingleCellCoadd]) -> pa.Table:
+    def process_cell(
+        self, single_cell_coadds: Sequence[SingleCellCoadd], rng,
+        simulate=False,
+    ) -> pa.Table:
         """Run metadetection on a single cell.
 
         Parameters
@@ -277,15 +312,267 @@ class MetadetectionShearTask(PipelineTask):
             Per-band, per-cell coadds, in the order specified by
             `MetadetectionShearConfig.required_bands`.
 
+        rng: `np.random.RandomState`
+            Random number generator.
+        simulate: bool, optional
+            If set to True, simulate the data
+
         Returns
         -------
         object_catalog : `pyarrow.Table`
             Output object catalog for the cell, with schema equal to
             `object_schema`.
         """
-        rows: list[dict[str, Any]] = []
+        from metadetect.lsst.metadetect import run_metadetect
+        from metadetect.lsst.metadetect import get_config as get_mdet_config
+
+        # rows: list[dict[str, Any]] = []
         # TODO: run metadetection on the cell, filling in 'rows' with
         # measurements.  Or replace 'rows' with a 'columns' dict of numpy array
         # columns and call 'from_pydict' instead of 'from_pylist' below, if
         # that's more convenient.
-        return pa.Table.from_pylist(rows, self.object_schema)
+
+        coadd_data = _simulate_coadd(rng)
+
+        mask_frac = _get_mask_frac(
+            coadd_data['mfrac_mbexp'],
+            trim_pixels=0,
+        )
+
+        mdet_config = get_mdet_config()
+        # TODO how to get the id/tract/patch_x etc. currently in schema
+        res = run_metadetect(
+            rng=rng,
+            config=mdet_config,
+            **coadd_data,
+        )
+        comb_res = _make_comb_data(
+            single_cell_coadd=single_cell_coadds[0],
+            res=res,
+            meas_type=mdet_config['meas_type'],
+            mask_frac=mask_frac,
+            full_output=True,
+        )
+
+        return comb_res
+        if len(comb_res) == 0:
+            return pa.Table.from_pylist(comb_res, self.object_schema)
+        else:
+            da = _dictify(comb_res)
+            return pa.Table.from_pydict(da, self.object_schema)
+
+    def _log_ids(self, single_cell_coadd):
+        idinfo = single_cell_coadd.identifiers
+        tract = idinfo.tract
+        patch_x = idinfo.patch.x
+        patch_y = idinfo.patch.y
+        cell_x = idinfo.cell.x
+        cell_y = idinfo.cell.y
+        mess = (
+            f'tract: {tract} patch: {patch_x},{patch_y} cell: {cell_x},{cell_y}'
+        )
+        self.log.info(mess)
+
+
+def _dictify(data):
+    output = {}
+    for name in data.dtype.names:
+        output[name] = data[name]
+    return output
+
+
+def _make_comb_data(
+    single_cell_coadd,
+    res,
+    meas_type,
+    mask_frac,
+    full_output=False,
+):
+    import esutil as eu
+
+    idinfo = single_cell_coadd.identifiers
+
+    add_dt = [
+        ('id', 'u8'),
+        ('tract', 'u4'),
+        ('patch_x', 'u1'),
+        ('patch_y', 'u1'),
+        ('cell_x', 'u1'),
+        ('cell_y', 'u1'),
+        ('shear_type', 'U2'),
+        ('mask_frac', 'f4'),
+        ('primary', bool),
+    ]
+
+    if not hasattr(res, 'keys'):
+        res = {'noshear': res}
+
+    dlist = []
+    for stype in res.keys():
+        data = res[stype]
+        if data is not None:
+
+            if not full_output:
+                data = _trim_output_columns(data, meas_type)
+
+            newdata = eu.numpy_util.add_fields(data, add_dt)
+
+            newdata['tract'] = idinfo.tract
+            newdata['patch_x'] = idinfo.patch.x
+            newdata['patch_y'] = idinfo.patch.y
+            newdata['cell_x'] = idinfo.cell.x
+            newdata['cell_y'] = idinfo.cell.y
+
+            if stype == 'noshear':
+                newdata['shear_type'] = 'ns'
+            else:
+                newdata['shear_type'] = stype
+
+            dlist.append(newdata)
+
+    if len(dlist) > 0:
+        output = eu.numpy_util.combine_arrlist(dlist)
+        # output['id'] = np.arange(output.size)
+    else:
+        output = []
+
+    return output
+
+
+def _trim_output_columns(data, meas_type):
+    # TODO decide what to keep
+    raise NotImplementedError('implement trim output')
+
+    # if meas_type == 'admom':
+    #     meas_type = 'am'
+    #
+    # # note the bmask/ormask compress to nothing
+    # cols2keep_orig = [
+    #     get_flags_name(data=data, meas_type=meas_type),
+    #     'bmask',
+    #     'ormask',
+    #     'row', 'row0',
+    #     'col', 'col0',
+    #     'mfrac',
+    #     '%s_s2n' % meas_type,
+    #     '%s_T_ratio' % meas_type,
+    #     '%s_g' % meas_type,
+    #     '%s_g_cov' % meas_type,
+    # ]
+    #
+    # cols2keep = []
+    # for col in cols2keep_orig:
+    #     if col in data.dtype.names:
+    #         cols2keep.append(col)
+    #
+    # return eu.numpy_util.extract_fields(data, cols2keep)
+
+
+def _get_mask_frac(mfrac_mbexp, trim_pixels=0):
+    """
+    get the average mask frac for each band and then return the max of those
+    """
+
+    mask_fracs = []
+    for mfrac_exp in mfrac_mbexp:
+        mfrac = mfrac_exp.image.array
+        dim = mfrac.shape[0]
+        mfrac = mfrac[
+            trim_pixels:dim - trim_pixels - 1,
+            trim_pixels:dim - trim_pixels - 1,
+        ]
+        mask_fracs.append(mfrac.mean())
+
+    return max(mask_fracs)
+
+
+def _simulate_coadd(rng):
+    from descwl_shear_sims.sim import (
+        make_sim,
+        get_sim_config,
+        get_se_dim,
+    )
+    from descwl_shear_sims.galaxies import make_galaxy_catalog
+    from descwl_shear_sims.psfs import make_fixed_psf, make_ps_psf, make_rand_psf
+    # from descwl_shear_sims.stars import make_star_catalog
+    from descwl_coadd.coadd_nowarp import make_coadd_nowarp
+    from metadetect.lsst.util import extract_multiband_coadd_data
+
+    g1, g2 = 0.02, 0.00
+
+    sim_config = get_sim_config()
+    if sim_config['se_dim'] is None:
+        sim_config['se_dim'] = get_se_dim(
+            coadd_dim=sim_config['coadd_dim'],
+            dither=sim_config['dither'],
+            rotate=sim_config['rotate'],
+        )
+
+    if sim_config['gal_type'] != 'wldeblend':
+        gal_config = sim_config.get('gal_config', None)
+    else:
+        sim_config["layout"] = None
+        gal_config = None
+
+    galaxy_catalog = make_galaxy_catalog(
+        rng=rng,
+        gal_type=sim_config['gal_type'],
+        coadd_dim=sim_config['coadd_dim'],
+        buff=sim_config['buff'],
+        layout=sim_config['layout'],
+        sep=sim_config['sep'],  # for layout='pair'
+        gal_config=gal_config,
+    )
+    if sim_config['psf_type'] == 'ps':
+        psf = make_ps_psf(
+            rng=rng,
+            dim=sim_config['se_dim'],
+            variation_factor=sim_config['psf_variation_factor'],
+        )
+    elif sim_config['randomize_psf']:
+        psf = make_rand_psf(
+            psf_type=sim_config["psf_type"], rng=rng,
+        )
+    else:
+        psf = make_fixed_psf(psf_type=sim_config["psf_type"])
+
+    sim_data = make_sim(
+        rng=rng,
+        galaxy_catalog=galaxy_catalog,
+        coadd_dim=sim_config['coadd_dim'],
+        se_dim=sim_config['se_dim'],
+        g1=g1,
+        g2=g2,
+        psf=psf,
+        draw_stars=sim_config['draw_stars'],
+        psf_dim=sim_config['psf_dim'],
+        dither=sim_config['dither'],
+        rotate=sim_config['rotate'],
+        bands=sim_config['bands'],
+        epochs_per_band=sim_config['epochs_per_band'],
+        noise_factor=sim_config['noise_factor'],
+        cosmic_rays=sim_config['cosmic_rays'],
+        bad_columns=sim_config['bad_columns'],
+        star_bleeds=sim_config['star_bleeds'],
+        sky_n_sigma=sim_config['sky_n_sigma'],
+    )
+
+    bands = list(sim_data['band_data'].keys())
+    exps = sim_data['band_data'][bands[0]]
+    assert (
+        not sim_config['dither']
+        and not sim_config['rotate']
+        and len(exps) == 1
+    )
+
+    coadd_data_list = [
+        make_coadd_nowarp(
+            exp=exps[0],
+            psf_dims=sim_data['psf_dims'],
+            rng=rng,
+            remove_poisson=False,
+        )
+        for band in bands
+    ]
+
+    return extract_multiband_coadd_data(coadd_data_list)
