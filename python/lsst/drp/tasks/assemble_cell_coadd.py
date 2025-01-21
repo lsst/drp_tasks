c1010afac330a17b377d7f6333eac055171b2dcc
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2025-01-21 12:35:22 -0500

    Include EDGE in the remove list

--- python/lsst/drp/tasks/assemble_cell_coadd.py
+++ python/lsst/drp/tasks/assemble_cell_coadd.py
@@ -112,7 +112,7 @@ class AssembleCellCoaddConfig(PipelineTaskConfig, pipelineConnections=AssembleCe
     )
     remove_mask_planes = ListField[str](
         doc="Mask planes to remove before coadding",
-        default=["NOT_DEBLENDED"],
+        default=["NOT_DEBLENDED", "EDGE"],
     )
     calc_error_from_input_variance = Field[bool](
         doc="Calculate coadd variance from input variance by stacking "
