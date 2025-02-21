84b05ff1b523d54707b8c32c5d9508f6d4e79cee
fred3m <fred.moolekamp@gmail.com>
2025-02-21 09:19:16 -0800

    Deprecate ScaleZeroPoint in assemble_coadd
    ScaleZeroPoint is deprecated and the default is changed to
    use PhotCalib(1.0), which ensures that the warps and coadds
    are in nJy, the same units as the visit exposures.

--- python/lsst/drp/tasks/reprocess_visit_image.py
+++ python/lsst/drp/tasks/reprocess_visit_image.py
@@ -472,6 +472,7 @@ class ReprocessVisitImageTask(pipeBase.PipelineTask):
         if self.config.remove_initial_photo_calib:
             # Calibrate the image, so it's on the same units as the background.
             result.exposure.maskedImage = initial_photo_calib.calibrateImage(result.exposure.maskedImage)
+            result.exposure.metadata["BUNIT"] = "nJy"
         result.exposure.maskedImage -= background.getImage()
         if self.config.remove_initial_photo_calib:
             # Uncalibrate so that we do the measurements in instFlux, because
