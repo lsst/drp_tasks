a4d68b4cbdfc57870440144a620da60b8acb1985
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2025-04-21 11:18:32 -0400

    Pass padding parameter when constructing the grid

--- python/lsst/drp/tasks/assemble_cell_coadd.py
+++ python/lsst/drp/tasks/assemble_cell_coadd.py
@@ -314,8 +314,13 @@ class AssembleCellCoaddTask(PipelineTask):
         """
         # grid has no notion about border or inner/outer boundaries.
         # So we have to clip the outermost border when constructing the grid.
-        grid_bbox = skyInfo.patchInfo.outer_bbox.erodedBy(skyInfo.patchInfo.getCellBorder())
-        grid = UniformGrid.from_bbox_cell_size(grid_bbox, skyInfo.patchInfo.getCellInnerDimensions())
+        padding = skyInfo.patchInfo.getCellBorder()
+        grid_bbox = skyInfo.patchInfo.outer_bbox.erodedBy(padding)
+        grid = UniformGrid.from_bbox_cell_size(
+            grid_bbox,
+            skyInfo.patchInfo.getCellInnerDimensions(),
+            padding=padding,
+        )
         return grid
 
     def _construct_grid_container(self, skyInfo, statsCtrl):
