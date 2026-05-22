097b8721c22a2316e49eaf72e332de7c4e2bc8fa
jrmullaney <j.mullaney@sheffield.ac.uk>
2026-05-22 15:18:47 +0100

    Skip non-centre warps before stacking
    Only warps that cover the central pixel can contribute to the PSF. So
    to ensure the coadd and the PSFs are consistent, we only want to coadd
    warps that also cover the central pixel.

--- python/lsst/drp/tasks/assemble_cell_coadd.py
+++ python/lsst/drp/tasks/assemble_cell_coadd.py
@@ -800,6 +800,18 @@ class AssembleCellCoaddTask(PipelineTask):
                     )
                     continue
 
+                psf_weight = weight * unmasked_fraction
+                overlaps_center = (
+                    detector_map[geom.Point2I(bbox.getCenter())] == ccd_row["ccd"]
+                ) and (psf_weight > 0.0)
+                if not overlaps_center:
+                    self.log.debug(
+                        "%s does not overlap with the center of the cell %s",
+                        warp_input.dataId,
+                        cellInfo.index,
+                    )
+                    continue
+
                 # Decide if a deep copy is necessary to apply the single
                 # detector cuts since it involves modifying the image in-place.
                 # If within the inner cell, there are three or more different
@@ -864,11 +876,6 @@ class AssembleCellCoaddTask(PipelineTask):
                         psf_eval_point,
                     )
 
-                psf_weight = weight * unmasked_fraction
-                overlaps_center = (
-                    detector_map[geom.Point2I(bbox.getCenter())] == ccd_row["ccd"]
-                ) and (psf_weight > 0.0)
-
                 observation_identifier = ObservationIdentifiers.from_data_id(
                     warp_input.dataId,
                     backup_detector=int(ccd_row["ccd"]),
@@ -887,14 +894,6 @@ class AssembleCellCoaddTask(PipelineTask):
                         psf_weight,
                         cellInfo,
                     )
-
-                if overlaps_center is False:
-                    self.log.debug(
-                        "%s does not overlap with the center of the cell %s",
-                        warp_input.dataId,
-                        cellInfo.index,
-                    )
-                    continue
 
                 # Everything below this has to do with the center of the cell
                 calexp_point = ccd_row.getWcs().skyToPixel(cell_centers_sky[cellInfo.index])
